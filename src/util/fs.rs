ec523d280121f95cf7e9e075de94d1870473b772
arm1nt <armin.nikbakht@gmail.com>
2025-06-29 20:33:09 +0200

    Create utility file with fs utility

---
+++ src/util/fs.rs
@@ -0,0 +1,23 @@
+use std::fs::{DirEntry, Metadata};
+use std::io::Error;
+use std::path::PathBuf;
+use crate::util::error_exit;
+
+pub fn get_metadata(path: &PathBuf) -> Metadata {
+    path.metadata().unwrap_or_else(|e| {
+        error_exit(Some(format!("Unable to get metadata for '{:?}: {e:?}", path)));
+    })
+}
+
+pub fn get_dir_entry(path: &PathBuf, entry_res: Result<DirEntry, Error>) -> DirEntry {
+    match entry_res {
+        Ok(dir_entry) => dir_entry,
+        Err(e) => {
+            error_exit(Some(format!("Unable to unwrap entry in directory '{:?}': {e:?}", path)))
+        }
+    }
+}
+
+pub fn is_supported_filetype(path: &PathBuf) -> bool {
+    path.is_dir() || path.is_file()
+}
